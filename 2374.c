24d5cc6ca5493c2e6e4f4c190112a1cc970d9af5
Matheus Cahú <matheus.cahu@unifesp.br>
2025-02-04 13:43:44 -0300

    Update 2374.c

--- 2374.c
+++ 2374.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
- 
+
 int main() {
-int n1, n2;
-scanf("%d", &n1);
-scanf("%d", &n2);
-printf("%d\n", n1-n2);
- 
+    int n1, n2;
+    scanf("%d", &n1);
+    scanf("%d", &n2);
+    printf("%d\n", n1 - n2);
+
     return 0;
 }
