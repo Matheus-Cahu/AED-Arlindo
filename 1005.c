ac9419267b7cc57d4d458f22622d1e1e6a90aa4d
Matheus Cahú <matheus.cahu@unifesp.br>
2025-02-04 13:49:17 -0300

    Update 1005.c

--- 1005.c
+++ 1005.c
@@ -1,16 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    double A, B;
-
-    // Lê os dois valores com uma casa decimal
-    scanf("%lf %lf", &A, &B);
-
-    // Calcula a média ponderada
-    double MEDIA = (A * 3.5 + B * 7.5) / 11.0;
-
-    // Imprime a média com o formato correto
+    double A, B, MEDIA;
+    scanf("%lf", &A);
+    scanf("%lf", &B);
+    MEDIA = (A * 3.5 + B * 7.5) / 11;
     printf("MEDIA = %.5lf\n", MEDIA);
-
     return 0;
 }
