aef875a498e6ca596145a563be774260f01e2fce
Matheus Cahú <matheus.cahu@unifesp.br>
2025-02-04 13:43:15 -0300

    Update 2413.c

--- 2413.c
+++ 2413.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
- 
+
 int main() {
- int clique;
- scanf("%d", &clique);
- printf("%d\n", clique*4);
+    int clique;
+    scanf("%d", &clique);
+    printf("%d\n", clique * 4);
     return 0;
 }
